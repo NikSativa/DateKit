a3978093ede3f187a5b9397293bfc2870938c21c
Mykyta Konopelko <nik.sativa@gmail.com>
2024-04-12 13:24:44 +0200

    dependencies updated

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "DateKit", targets: ["DateKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/NikSativa/SpryKit.git", .upToNextMajor(from: "2.2.0"))
+        .package(url: "https://github.com/NikSativa/SpryKit.git", .upToNextMajor(from: "2.2.2"))
     ],
     targets: [
         .target(name: "DateKit",
